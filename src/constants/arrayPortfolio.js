5b0c48e58e0ca68bbc3b32b122b78f822b6710e3
Zakhar Menshikov <zm681986@mail.ru>
2022-12-15 23:34:55 +0500

    about

--- src/constants/arrayPortfolio.js
+++ src/constants/arrayPortfolio.js
@@ -1,5 +1,13 @@
-import slide1 from '../images/phone_2.png';
-
+import slide1 from '../images/calculator/1.png';
+import slide2 from '../images/calculator/2.png';
+import slide3 from '../images/calculator/3.png';
+import slide4 from '../images/calculator/4.png';
+import slide5 from '../images/calculator/5.png';
+import slide6 from '../images/calculator/6.png';
+import slide7 from '../images/calculator/7.png';
+import slide8 from '../images/calculator/8.png';
+import slide9 from '../images/calculator/9.png';
+import slide11 from '../images/calculator/11.png';
 export const toppings = [
     {
         name: "Сталь СТЗ, 2мм",
@@ -15,78 +23,122 @@ export const toppings = [
     {
         name: "Нержавеющая сталь AISI 304, 2мм",
         price: 102000,
-        src: slide1
+        src: slide2
     },
     {
         name: "Отделка деревом",
         price: 15000,
-        src: slide1
+        src: slide3
     },
     {
         name: "Подставка каркас",
         price: 9000,
-        src: slide1
+        src: slide4
     },
     {
         name: "Подставка ветрозащита",
         price: 20000,
-        src: slide1
+        src: slide5
     },
     {
         name: "Подставка печь",
         price: 37000,
-        src: slide1
+        src: slide6
     },
     {
         name: "Дымоход из нержавейки",
         price: 5000,
-        src: slide1
+        src: slide7
     },
 
     {
         name: "Лестница из сосны подставная",
         price: 5000,
-        src: slide1
+        src: slide8
     },
     {
         name: "Лестница с подставкой из металлокаркаса",
         price: 37000,
-        src: slide1
+        src: slide9
     },
+
+    {
+        name: "Крышка",
+        price: 12000,
+        src: slide11
+    },
+
     {
         name: "Столик из нержавеющей стали",
         price: 12000,
         src: slide1
     },
-    {
-        name: "Крышка",
-        price: 12000,
-        src: slide1
-    }
 ];
 
 
 export const top = [
     {
-        name: "ААААААА",
-        price: 1.2,
+        name: "Сталь СТЗ, 2мм",
+        price: 33000,
         src: slide1
 
     },
     {
-        name: "РРРРРР",
-        price: 2.0,
+        name: "Сталь СТЗ, 3мм",
+        price: 40000,
         src: slide1
     },
     {
-        name: "ЩЩЩЩЩЩ",
-        price: 2.5,
-        src: slide1
+        name: "Нержавеющая сталь AISI 304, 2мм",
+        price: 102000,
+        src: slide2
     },
     {
-        name: "ТТТТТТ",
-        price: 3.0,
-        src: slide1
+        name: "Отделка деревом",
+        price: 15000,
+        src: slide3
+    },
+    {
+        name: "Подставка каркас",
+        price: 9000,
+        src: slide4
+    },
+    {
+        name: "Подставка ветрозащита",
+        price: 20000,
+        src: slide5
+    },
+    {
+        name: "Подставка печь",
+        price: 37000,
+        src: slide6
+    },
+    {
+        name: "Дымоход из нержавейки",
+        price: 5000,
+        src: slide7
     },
 
+    {
+        name: "Лестница из сосны подставная",
+        price: 5000,
+        src: slide8
+    },
+    {
+        name: "Лестница с подставкой из металлокаркаса",
+        price: 37000,
+        src: slide9
+    },
+
+    {
+        name: "Крышка",
+        price: 12000,
+        src: slide11
+    },
+
+    {
+        name: "Столик из нержавеющей стали",
+        price: 12000,
+        src: slide1
+    },
 ];
