8c672b8fa1589c62e70a76e59b7782683e95752b
Zakhar Menshikov <zm681986@mail.ru>
2022-12-23 23:18:08 +0500

    Catalog

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -30,14 +30,9 @@ function Navigation() {
                         isActive ? 'nav__link-2 active' : 'nav__link-2'
                     }
                 >
-                    <img className='menu_icon' src={menu_icon} alt='' /> Каталог
+                    Каталог
                 </NavLink>
-                <ul className="submenu">
-                    <li className="submenu__links"><Link className="submenu__link" to="/services_1">Окрашивание</Link></li>
-                    <li className="submenu__links"><Link className="submenu__link" to="/services_2">Тонирование</Link></li>
-                    <li className="submenu__links"><Link className="submenu__link" to="/services_3">Женские стрижки</Link></li>
-                    <li className="submenu__links"><Link className="submenu__link" to="/services_4">Профессиональный уход</Link></li>
-                </ul>
+
                 <NavLink
                     to='/delivery'
                     className={({ isActive }) =>
