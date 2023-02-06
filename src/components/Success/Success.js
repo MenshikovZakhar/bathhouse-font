bb58806700f4c37f7095956cabe73b82ec36d5cf
Zakhar Menshikov <zm681986@mail.ru>
2023-02-06 22:23:41 +0500

    ssr

--- src/components/Success/Success.js
+++ src/components/Success/Success.js
@@ -2,21 +2,21 @@ import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import './Success.css';
 function Success() {
     return (
         <>
             <Header
             />
-            <div className="page-notfound">
-                <h3 className="notfound__title">
+            <div className="success">
+                <h3 className="success__title">
                     Заявка отправлена
                 </h3>
-                <p className="notfound__subtitle">
-                    'Ваше сообщение успешно доставлено. Мы свяжемся с Вами в ближайшее время.'
+                <p className="success__subtitle">
+                    Ваше сообщение успешно доставлено. Мы свяжемся с Вами в ближайшее время.
                 </p>
-                <Link className="notfound__link" to="/">
-                    Вернуться на главную
+                <Link className="success__link" to="/">
+                    Вернуться на главную страницу
                 </Link>
             </div>
             <Footer />
